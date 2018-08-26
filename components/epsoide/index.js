22867cf3dcbb9e7b84910e96b166a07e21166f8d
WenChaoGit <1031229910@qq.com>
2018-08-26 14:02:28 +0800

    navi

--- components/epsoide/index.js
+++ components/epsoide/index.js
@@ -4,16 +4,42 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    index: {
+      type: Number,
+      observer(newValue) {
+        let val = newValue < 10 ? '0' + newValue : newValue
+        this.setData({
+          _index: val
+        })
+      }
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    months: ['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月',
+      '十一月',
+      '十二月',
+    ],
+    year: 0,
+    month: '',
+    _index: ''
   },
 
+  /**
+   * 生命周期函数，组件挂载到页面时触发
+   */
+  attached() {
+    let date = new Date()
+    let year = date.getFullYear()
+    let month = date.getMonth()
+    this.setData({
+      year,
+      month:this.data.months[month]
+    })
+  },
   /**
    * 组件的方法列表
    */
